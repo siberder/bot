20fbc7a37ee33e1c0799c06bf1e193515a250fc2
siberder <cool.kozhin@yandex.ru>
2018-01-27 18:44:41 +0300

    Added new module wishreporter, added style to wishreport

--- commands/wishview.py
+++ commands/wishview.py
@@ -1,12 +1,15 @@
 import command_system
 import wish
+import wishreporter
 import vkapi
 
 def getWishes(uid, body):
    message = 'Текущие пожелания: '
    message += str(len(wish.wishes))
-   wishesDoc = wish.getWishesReport(wish.wishes)
+
+   wishesDoc = wishreporter.getWishesReport(wish.wishes)
    attachment = vkapi.upload_document(uid, wishesDoc)
+
    return message, attachment
 
 cat_command = command_system.Command()
--- wish.py
+++ wish.py
@@ -1,8 +1,5 @@
 import jsonpickle
 import datetime
-from weasyprint import HTML
-from yattag import Doc
-
 
 class WishDayHours:
 	anyhours = ["Any", "any"]
@@ -147,88 +144,7 @@ def addFakeWishes(count):
 	print("Adding %s fake wishes" % count)
 
 	for x in range(0, count):
-		addWish("Name Surname", count, getTestWish())
-
-def generateWishesHTML(wsh):
-	print("Generating wishes HTML..")
-
-	doc, tag, text = Doc().tagtext()
-
-	doc.asis('<!DOCTYPE html>')
-
-	with tag('head'):
-		doc.asis('<meta charset=utf-8>')
-
-	with tag('body'):
-		with tag('h1'):
-			text("Пожелания")
-
-		with tag('table'):
-			with tag('tr'):
-				headrows = ["Имя", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье", "Понедельник", "Комментарий"]
-
-				for hrow in headrows:
-					with tag('th'):
-						text(hrow)
-
-			with tag("tr"):
-				pass
-
-			for w in wsh:
-				with tag("tr"):
-					with tag("td"):
-						text(w.name)
-
-					for x in range(0, 7):
-						daysInWish = len(w.days)
-
-						with tag("td"):
-							if x < daysInWish and w.days[x] is not None:
-								text(str(w.days[x]))
-
-					with tag("td"):
-						text(w.comment)
-
-	rdyDoc = doc.getvalue()
-
-	with open("lastwishes.html", "w+") as f:
-		f.write(rdyDoc)
-
-	return rdyDoc	
-
-def getWishesHTMLFile():
-	try:
-		return open("wishes.html", "rb")
-	except FileNotFoundError:
-		generateWishesHTML(wishes)
-		getWishesHTMLFile()
-
-def generateWishesPDF(htmlstring):
-	print("Generating wishes PDF..")
-
-	path = 'wishes.pdf'
-	# pdfkit.from_string(htmlstring, path) 
-	HTML(string=htmlstring).write_pdf(path)
-	return open(path, "rb")
-
-def getWishesPDFFile():
-	try:
-		return open("wishes.pdf", "rb")
-	except FileNotFoundError:
-		generateWishesPDF(getWishesHTMLFile())
-		getWishesPDFFile()
-
-def getWishesReport(wishes):
-	print("Getting wishes report..")
-
-	try:
-		html = generateWishesHTML(wishes)
-
-		pdfRB = generateWishesPDF(html)
-		return pdfRB
-	except:
-		return getWishesPDFFile()
-	
+		addWish("Name Surname", count, getTestWish())	
 
 wishPath = "wishes.json"
 
