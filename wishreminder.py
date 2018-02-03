c6c48a2587d562c5c5c28d8bce4c87231f372e6b
siberder <cool.kozhin@yandex.ru>
2018-02-03 16:56:08 +0300

    Fixing wish reminder

--- wishreminder.py
+++ wishreminder.py
@@ -7,15 +7,27 @@ import time
 import utils
 
 def getRemindMembers():
-	return list(set(vkapi.getGroupMembers()) - set(settings.ignoreids))
+	groupMembers = vkapi.getGroupMembers()
+	print("Group members: " + str(groupMembers))
+
+	ignoreMembers = settings.ignoreids
+	print("Ignore members: " + str(ignoreMembers))
+
+	resultMembers = list(set(groupMembers) - set(ignoreMembers))
+	print("Remind members: " + str(resultMembers))
+
+	return resultMembers
 
 def getUsersWithoutWishes():
 	report = wishreporter.getWishesReport(wish.wishes, wishreporter.WishFilterSettings(date = utils.getCurTue()))
 
 	userWithWishes = [u.uid for u in report.wishes]
+	print("Users with wishes: " + str(userWithWishes))
+
 	allPeople = getRemindMembers()
 
 	usersWithoutWishes = list(set(allPeople) - set(userWithWishes))
+	print("Users without wishes: " + str(usersWithoutWishes))
 
 	print("{0} users found without wishes".format(len(usersWithoutWishes)))
 
