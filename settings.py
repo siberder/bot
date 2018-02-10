f1ae87a8378d341393dd93f994c2dbdd789d9104
siberder <cool.kozhin@yandex.ru>
2018-02-10 21:03:21 +0300

    Added manager view function

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
-#token = 'b882cd131dc1e045642e3127169324fe8ddb1eef67ff8a8bbb000cb4a17b5a0bcac50287c8bbbf2e179a3'
-token = '7aa98202ff3d3c4e8f83c987079056eb16874239d68ddc5b9d9a5370e47196568f33a3c6439aed67ed5d6'
+token = 'b882cd131dc1e045642e3127169324fe8ddb1eef67ff8a8bbb000cb4a17b5a0bcac50287c8bbbf2e179a3'
+#token = '7aa98202ff3d3c4e8f83c987079056eb16874239d68ddc5b9d9a5370e47196568f33a3c6439aed67ed5d6'
 confirmation_token = 'f75d26cf'
 
 wishesPath = "wishes.json"
 
-#group_id = 52421856
-group_id = 155271106
+group_id = 52421856
+#group_id = 155271106
 
 ignoreids = []
 ignoreManagers = True
